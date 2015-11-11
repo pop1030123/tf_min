e0cda57868a224d7988bcea3340542f30d18019a
fupeng <fupeng@songshuyun.com>
2015-11-11 15:00:34 +0800

    note file
    add note file

---
+++ app/src/main/java/com/pop/tf_min/entity/Note.java
@@ -0,0 +1,18 @@
+package com.pop.tf_min.entity;
+
+import java.util.List;
+
+/**
+ * Created by pengfu on 15/11/10.
+ */
+public class Note {
+
+
+
+
+
+
+    private String content ;
+    private Note parentNote ;
+    private List<Note> childrenNote ;
+}
